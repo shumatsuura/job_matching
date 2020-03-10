eb23c6c81f01d185668bf094cfeb8a5ca6b8c0b7
shumatsuura <shunsuke.mtr@gmail.com>
2020-03-10 14:25:02 +0900

    [change]change data format of_phone_number from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_120746) do
+ActiveRecord::Schema.define(version: 2020_03_10_052228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_120746) do
     t.datetime "date_of_birth"
     t.string "gender"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "race"
     t.string "religion"
     t.integer "expected_salary"

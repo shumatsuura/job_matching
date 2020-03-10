53a09f8e376a30ed8b38ff74cf33a8351df6de9f
shumatsuura <shunsuke.mtr@gmail.com>
2020-03-10 10:01:31 +0900

    user controller生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'statics#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+  resources :users, only:[:show,:index]
+
 end

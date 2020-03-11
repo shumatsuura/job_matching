cde82464df70a9d513fe2b9b8ed33936958fd88e
shumatsuura <shunsuke.mtr@gmail.com>
2020-03-11 13:09:38 +0900

    [change]educationの追加のコードをcoffeeからjqueryに修正

---
+++ app/assets/javascripts/form.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function(){
+  $('form').on('click', '.remove_fields', function(event){
+    $(this).prev('input[type=hidden]').val('1');
+    $(this).closest('fieldset').hide();
+    event.preventDefault();
+  });
+
+  $('form').on('click', '.add_fields', function(event){
+    var time = new Date().getTime();
+    var regexp = new RegExp($(this).data('id'), 'g');
+    $(this).before($(this).data('fields').replace(regexp, time));
+    event.preventDefault();
+    });
+  });

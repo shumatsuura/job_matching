2309ccaaab7e39b77b05179ea8bc380d6ee908e2
shumatsuura <shunsuke.mtr@gmail.com>
2020-03-11 16:29:14 +0900

    [add]Industryモデル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_064548) do
+ActiveRecord::Schema.define(version: 2020_03_11_072404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2020_03_11_064548) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_educations_on_user_id"
+  end
+
+  create_table "industries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "languages", force: :cascade do |t|
---
+++ spec/factories/industries.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :industry do
+    string { "" }
+  end
+end

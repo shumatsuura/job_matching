7d732498b87a4df511b39265297f84bf78d6a85e
shumatsuura <shunsuke.mtr@gmail.com>
2020-03-11 15:21:57 +0900

    [add]educationを追加した時のインデックスが正常に表示されるようにアップデート

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -6,9 +6,16 @@ $(document).on('turbolinks:load', function(){
   });
 
   $('form').on('click', '.add_fields', function(event){
+    education_index = Number($('.educations_index:last').text()) + 1;
+
     var time = new Date().getTime();
     var regexp = new RegExp($(this).data('id'), 'g');
     $(this).before($(this).data('fields').replace(regexp, time));
     event.preventDefault();
+
+    $('.educations_index:last').text(education_index);
+
     });
   });
+
+// .replace(regexp, time)

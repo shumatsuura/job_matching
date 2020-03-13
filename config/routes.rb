f10e4a2b306dfedc9726325298be4387a2236ee8
shumatsuura <shunsuke.mtr@gmail.com>
2020-03-13 16:21:59 +0900

    [add]companyモデル生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'statics#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :companies, controllers: {
+        sessions: 'companies/sessions'
+      }
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_041445) do
+ActiveRecord::Schema.define(version: 2020_03_13_064913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_companies_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
+  end
 
   create_table "desired_industries", force: :cascade do |t|
     t.integer "user_id", null: false

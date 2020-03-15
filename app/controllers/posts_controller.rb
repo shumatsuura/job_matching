0215953562cb1ce68ab231a11e842e7a24eff63c
shumatsuura <shunsuke.mtr@gmail.com>
2020-03-15 23:29:23 +0900

    [add]ビューを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only:[:edit,:update,:show]
+  before_action :set_post, only:[:edit,:update,:show,:destroy]
 
   def new
     @post = current_company.posts.build
@@ -30,6 +30,11 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to dashboard_company_path(@post.company.id), notice: "削除しました。"
   end
 
   private

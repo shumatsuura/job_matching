0c98bf0103ef8705abff29b564baad9825757c7b
shumatsuura <shunsuke.mtr@gmail.com>
2020-04-18 23:59:01 +0900

    [update]scout message design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user_and_company
     if not user_signed_in? || company_signed_in?
-      redirect_to root_path, notice: "You need to sign in or sign up before continuing."
+      redirect_to root_path, alert: "You need to sign in or sign up before continuing."
     end
   end
 
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def ensure_admin_user
-    redirect_to root_path, notice: "No Access Right." unless admin_user?
+    redirect_to root_path, alert: "No Access Right." unless admin_user?
   end
 
   def authenticate_company_without_admin_user
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
   def production?
     Rails.env.production?
   end
-  
+
   def basic
     authenticate_or_request_with_http_basic('BA') do |name, password|
       name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
